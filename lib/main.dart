aa52c81f3a6104fada4952726a4d055c853219f5
Jephin John <jephinjohn85@gmail.com>
2022-02-04 20:58:58 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+main(){
+ runApp(firstflutterapp());
+}
+
+class firstflutterapp extends StatelessWidget {
+  const firstflutterapp({Key ? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        primaryColor: Colors.blue,
+      ),
+      home: homescreen()
+    );
+  }
+}
+
+class homescreen extends StatelessWidget {
+  const homescreen({Key? key}) : super(key : key);
+
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      backgroundColor: Colors.green,
+      appBar : AppBar(backgroundColor: Colors.black,)
+    );
+  }
+}
